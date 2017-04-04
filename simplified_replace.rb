fb5e70186c6ae8137ac16f373339e8d774bcbf05
MichaelDimmitt <MichaelGDimmitt@gmail.com>
2017-04-04 16:03:45 -0400

    core function seems correct

--- simplified_replace.rb
+++ simplified_replace.rb
@@ -1,35 +1,101 @@
-# puts f_name
+#!/usr/bin/env ruby
+
+
 def write_to_end
   File.open("Readme2.md", "a+") do |f|
-  f.write("hello333\n");
-end
-end
-def write_to_beginning
-#this will start at the beginning go through each line and replace. 
-  File.open("Readme2.md", "r+") do |f|
-  #      old_pos = 0
-  #     f.each do |line|
-  #        f.pos = old_pos   # this is the 'rewind'
-          #f.print line.gsub(/GxUserContacts/, "GxUserContact")
-  f.write("hello\n") #replaces that line.
-   # also f.syswrite("")
-   # f.syswrite worse until writes become 1000 characters large.
-   #       old_pos = f.pos
-   #   end
+    f.write("hello333\n");
   end
 end
-def write_to_line
+def write_to_all        # this will start at the beginning go through each line and replace.
   File.open("Readme2.md", "r+") do |f|
-    old_pos = 0
-  
+  f.write("qqqqq") #replaces that line.
     f.each do |line|
-    f.pos = old_pos   # this is the 'rewind'
-    if(pos == 3)f.write("hello")end
-
-  
+    f.write("qqqqq") #replaces that line.
+   end
+  end
 end
 
+def testsd
+File.open("Readme2.md", "r+") do |f|
+    f.each do |line|
+  puts "position 1 #{f.pos}"
+    i = 1;
 
+start = f.gets(1)
+    if(start == "#")
+      while(f.gets(1) == "#")
+        puts "success"
+      end
+      track = f.pos
+      f.pos = track -1
+      if(f.getc != "\s")
+        print true  #not a space
+        f.pos = track -1
+        strin = f.gets
+        strin2 = " #{strin}" #adds a space.
+        f.pos = track - 1
+        f.write(strin2)
+      else
+        print false # a space
+      end
+      #add a space...
+       #and now we are in the spot.
+       # if no space add a space
+   #read first
+   #then concatenate space to beginning.
+    end
+      puts "position 2 #{f.pos}"
+    end
+  end
+end
 
-write_to_end
-write_to_beginning
+def puts_all        # this will start at the beginning go through each line and replace.
+  File.open("Readme2.md", "r+") do |f|
+  #puts(f.gets(1));
+  old_pos = 0
+  i = 1;
+    f.each do |line|
+      f.write("hello") #replaces that line.
+      end
+  end
+end
+def write_to_beginning # this will start at the beginning go through each line and replace.
+  File.open("Readme2.md", "r+") do |f|
+      f.write("pppp") #replaces that line.
+  end
+end
+
+def write_to_pos_all_lines #complete
+  File.open("Readme2.md", "r+") do |f|
+  f.pos = 3
+  f.write("robertTHeDOIG") #replaces that line.
+    f.each do |line|
+#        line.pos = 3
+       f.pos += 2
+       f.write("robertTHeDOIG") #replaces that line.
+   end
+  end
+end
+
+def write_to_line #
+  File.open("Readme2.md", "r+") do |f|
+      old_pos = 0
+      f.each do |line|
+        f.pos = old_pos   # this is the 'rewind'
+        if(f.pos == 3)
+          f.write("robertTHeDOIG")
+          old_pos +=1
+        end
+          old_pos +=1
+      end
+      f.write("hi")
+  end
+end
+
+#write_to_line
+# write_to_end
+#write_to_pos_all_lines
+#puts_all
+testsd
+#puts_all
+# write_to_beginning
